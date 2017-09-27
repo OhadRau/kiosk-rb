d36b5c0cc1fc79441b439dafa599e6e16658b8e3
David William Fitzgerald <Dankerprouduct2@gmail.com>
2017-09-27 14:34:43 -0400

    tiny fix

--- routes/tickets.rb
+++ routes/tickets.rb
@@ -62,8 +62,8 @@ class Kiosk
       flash[:error] = "Ticket is missing required fields. Please try again."
       return redirect '/'
     end
-    
-    if params["body"].length < 40
+
+    if params[:"body"].length < 40
       flash[:error] = "Description must be at least 40 characters long" 
       return redirect '/' 
     end
